e6f72fa8bbe5e905554bb2a4f7f19c5c4d8c23a6
Jay Oldham-Smith <jay.oldhamsmith@outlook.com>
2024-06-02 23:15:54 +0100

    toil(UnitTests): Reformat test

--- src/lib/utils/validation.test.ts
+++ src/lib/utils/validation.test.ts
@@ -16,10 +16,17 @@ describe('validation', () => {
         }
     );
 
-    test.each([[null, false], [undefined, false], ['', false], ['  ', false], ['other', false], ['http://www.domain.com/path/path/?query=value', true], ['http://www.domain.com/path/path/image.png', true]])(
-        '.isValidURL(%s) => %s',
-        (a, expected) => {
-            expect(validation.isValidURL(a)).toBe(expected);
-        }
+    test.each([
+        [null, false],
+        [undefined, false],
+        ['', false],
+        ['  ', false],
+        ['other', false],
+        ['http://www.domain.com/path/path/?query=value', true],
+        ['http://www.domain.com/path/path/image.png', true]])(
+            '.isValidURL(%s) => %s',
+            (a, expected) => {
+                expect(validation.isValidURL(a)).toBe(expected);
+            }
     );
 });
